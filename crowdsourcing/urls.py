28de4c3bd6b7e3c88b288e5f51a69c828e661331
jsmullyan <none@none>
2009-04-20 19:45:59 +0000

    add patterns for the various result pages
    --HG--
    extra : convert_revision : svn%3Ae9d0083c-c949-11dd-86c2-0f8696b7b6f9/trunk%4023

--- crowdsourcing/urls.py
+++ crowdsourcing/urls.py
@@ -2,7 +2,12 @@ from __future__ import absolute_import
 
 from django.conf.urls.defaults import patterns, url
 
-from .views import survey_detail, survey_results, survey_results_json
+from .views import (survey_detail,
+                    survey_results,
+                    survey_results_json,
+                    survey_results_map,
+                    survey_results_archive,
+                    survey_results_aggregate)
 
 urlpatterns=patterns(
     "",
@@ -10,9 +15,21 @@ urlpatterns=patterns(
         survey_detail,
         name="survey_detail"),
 
-    url(r'^(?P<slug>[-a-z0-9_]+)/results/$',
+    url(r'^(?P<survey>[-a-z0-9_]+)/results/$',
         survey_results,
         name="survey_results"),
+
+    url(r'^(?P<survey>[-a-z0-9_]+)/results/map/$',
+        survey_results_map,
+        name="survey_results_map"),
+
+    url(r'^(?P<survey>[-a-z0-9_]+)/results/archive/$',
+        survey_results_archive,
+        name="survey_results_archive"),
+
+    url(r'^(?P<survey>[-a-z0-9_]+)/results/aggregate/$',
+        survey_results_aggregate,
+        name="survey_results_aggregate"),
 
     url(r'^(?P<survey>[-a-z0-9_]+)/api/$',
         survey_results_json,
