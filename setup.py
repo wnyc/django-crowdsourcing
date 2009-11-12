6576995b1b19ff66006e0a0de9f8f0edb9c28d83
dsmith@66.111.5.230.static.nyinternet.net <dsmith@66.111.5.230.static.nyinternet.net>
2009-11-12 13:59:23 -0500

    Setup script.

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
-# tbd
+from distutils.core import setup
+
+setup(name='Crowdsourcing',
+      version='1.0',
+      description='Django app for collecting and displaying surveys.',
+      author='Jacob Smullyan, Dave Smith',
+      author_email='jsmullyan@gmail.com',
+      url='http://code.google.com/p/django-crowdsourcing/',
+      packages=['crowdsourcing'],
+     )
