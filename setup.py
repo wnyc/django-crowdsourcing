24b20f10f77e45ab00582611561568009ee2aa17
Jacob Smullyan <jsmullyan@gmail.com>
2010-04-19 12:26:50 -0400

    version bump

--- setup.py
+++ setup.py
@@ -3,11 +3,18 @@ try:
 except ImportError:
     from distutils.core import setup
 
-setup(name='Crowdsourcing',
-      version='1.0',
+import os
+readme_file = os.path.join(os.path.dirname(__file__),
+                           'README')
+long_description = open(readme_file).read()    
+
+setup(name='django-crowdsourcing',
+      version='1.1',
       description='Django app for collecting and displaying surveys.',
+      long_description=long_description,
       author='Jacob Smullyan, Dave Smith',
       author_email='jsmullyan@gmail.com',
       url='http://code.google.com/p/django-crowdsourcing/',
       packages=['crowdsourcing'],
+      license='MIT',
      )
