e911ed0458d86d3ae9413a4cb7e3aaa6acb2e897
dsmith@localhost <dsmith@localhost>
2011-01-06 14:51:54 -0500

    Updated version.
    --HG--
    branch : takeaway

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ classifiers = [
 
 
 setup(name='django-crowdsourcing',
-      version='1.1.19',
+      version='1.1.20',
       classifiers=classifiers,
       description='Django app for collecting and displaying surveys.',
       long_description=long_description,
