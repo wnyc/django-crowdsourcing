0f9ba5b57bbbb448d1396e4d6ccd200c9d93c571
David Smith <dasmith2@gmail.com>
2013-02-01 15:40:11 -0500

    Notification email uses the thumbnail. 1.1.45

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ classifiers = [
 
 
 setup(name='django-crowdsourcing',
-      version='1.1.44',
+      version='1.1.45',
       classifiers=classifiers,
       description='Django app for collecting and displaying surveys.',
       long_description=long_description,
